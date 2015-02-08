4b61566ef6780e0c10aafafa0d3dd13989973995
Ishan <ishanatmuzaffarpur@gmail.com>
2015-02-08 16:53:18 +0530

    Cleaned out passport code

--- viewHandlers.js
+++ viewHandlers.js
@@ -1,9 +1,8 @@
 var fs = require('fs');
 var formidable = require('formidable');
 var util = require('util');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
+// Hardcoded user Information
 var users = [
     { useremail: 'ishan1608@gmail.com', userpassword: '123456'},
     { useremail: 'ishan1608@live.com', userpassword: 'qwerty'}
@@ -35,74 +34,13 @@ function notFound(req, res) {
 }
 
 
-
-
-
-// Hopefully a function to find whether an email is registered or not.
-function findByUseremail(useremail, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.useremail === useremail) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
-
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID (I am using email) when serializing, and finding
-//   the user by ID when deserializing.
-passport.serializeUser(function(user, done) {
-  done(null, user.useremail);
-});
-
-passport.deserializeUser(function(useremail, done) {
-  findByUseremail(useremail, function (err, user) {
-    done(err, user);
-  });
-});
-
-
-// Use the LocalStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a username and password), and invoke a callback
-//   with a user object.  In the real world, this would query a database;
-//   however, in this example we are using a baked-in set of users.
-passport.use(new LocalStrategy(
-  function(useremail, userpassword, done) {
-      console.log('local strategy is getting ' + useremail + ' : ' + userpassword);
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      
-      // Find the user by username.  If there is no user with the given
-      // username, or the password is not correct, set the user to `false` to
-      // indicate failure and set a flash message.  Otherwise, return the
-      // authenticated `user`.
-      findByUsername(useremail, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { message: 'Unknown user ' + useremail }); }
-        if (user.userpassword != userpassword) { return done(null, false, { message: 'Invalid password' }); }
-        return done(null, user);
-      })
-    });
-  }
-));
-
-
-
-
-
-
-
-
 // Local login using emailId and password combination
 function loginLocal(req, res) {
     console.log('loginLocal called');
-    // Not Working because passport authenticate only works as middleware, I think it only works with express
-    passport.authenticate('local', { successRedirect: '/dashboard', failureRedirect: '/' });
+    // Need to set cookies on my own. For now I will test using hardocded users
+    res.writeHead(302, {'Location': '/dashboard', 'Content-Type': 'text/plain; charset=utf-8'});
+    res.write('Redirecting.......');
+    res.end();
 }
 
 function dashboard(req, res) {
